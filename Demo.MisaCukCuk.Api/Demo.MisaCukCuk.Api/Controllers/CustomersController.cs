b93bf2696cc7915e76dc34ef8b0f0a8bc425e328
s2Shit91 <thangnd@tandan.com.vn>
2021-08-09 10:28:51 +0700

    Update

--- Demo.MisaCukCuk.Api/Demo.MisaCukCuk.Api/Controllers/CustomersController.cs
+++ Demo.MisaCukCuk.Api/Demo.MisaCukCuk.Api/Controllers/CustomersController.cs
@@ -21,7 +21,10 @@ namespace Demo.MisaCukCuk.Api.Controllers
         {
             // Truy cập vào db:
             // 1. Khai báo thông tin kết nối db:
-            var connectionString = "Host = 47.241.69.179;" + "Database = MISA.CukCuk_Demo_NVMANH;" + "User Id = dev;" + "Password = 12345678";
+            var connectionString = "Host = 47.241.69.179;" + 
+                "Database = MISA.CukCuk_Demo_NVMANH;" +
+                "User Id = dev;" + 
+                "Password = 12345678";
             
             // 2. Khởi tạo đối tượng kết nối với db:
             IDbConnection dbConnection = new MySqlConnection(connectionString);
@@ -32,6 +35,55 @@ namespace Demo.MisaCukCuk.Api.Controllers
 
             // 4. Trả về cho client:
             return Ok(customers);
+        }
+        [HttpPost]
+        public IActionResult InsertCustomer(Customer customer)
+        {
+            //Truy cập vào database:
+            // 1.Khai báo đối tượng
+            var connectionString = "Host = 47.241.69.179;" +
+                "Database = MISA.CukCuk_Demo_NVMANH;" +
+                "User Id = dev;" +
+                "Password = 12345678";
+            // 2.Khởi tạo đối tượng kết nối với database
+            IDbConnection dbConnection = new MySqlConnection(connectionString);
+            //khai báo dynamicParam:
+            var dynamicParam = new DynamicParameters();
+
+            // 3.Thêm dữ liệu vào database
+            var columnsName = string.Empty;
+            var columnsParam = string.Empty;
+
+            //Đọc từng property của object:
+            var properties = customer.GetType().GetProperties();
+
+
+            //Duyệt từng property:
+            foreach (var prop in properties)
+            {
+                //lấy tên của prop:
+                var propName = prop.Name;
+
+                //Lấy value của prop:
+                var propValue = prop.GetValue(customer);
+
+                //Lấy kiểu dữ liệu của prop:
+                var propType = prop.PropertyType;
+
+                //thêm param tương ứng với mỗi property của đối tượng
+                dynamicParam.Add($"@{propName}", propValue);
+
+                columnsName += $"@{propName},";
+                columnsParam += $"@{propName},";
+            }
+            columnsName = columnsName.Remove(columnsName.Length - 1, 1);
+            columnsParam = columnsParam.Remove(columnsParam.Length - 1, 1);
+            var sqlCommand = $"INSERT INTO Customer({columnsName}) VALUES({columnsParam}) ";
+
+            // Trả về cho client
+
+            var response = StatusCode(200, customer);
+            return response;
         }
     }
 }
